3c9bf7e51fb6bd2cfa02b627e4f7afbe337a9879
Charles Dufour <cdufour@xebia.fr>
2019-05-13 07:15:20 +0200

    exercice1

--- src/main/scala/fr/xebia/xke/frameless/Exercice1.scala
+++ src/main/scala/fr/xebia/xke/frameless/Exercice1.scala
@@ -8,13 +8,20 @@ import org.apache.spark.sql.Dataset
 object Exercice1 {
 
   def selectAllPlayers(matchs: Dataset[Match]): Seq[String] = {
-    val playersDataset = matchs.select("player1").union(matchs.select("player"))
+    val playersDataset = matchs.select("player1").union(matchs.select("player2"))
 
     playersDataset.collect().toSeq.map(_.getString(0))
   }
 
   def selectAllPlayersTyped(matchs: Dataset[Match]): Seq[String] = {
-    ???
+    val typedMatchs = TypedDataset.create(matchs)
+    val players = typedMatchs.project[Players]
+
+    players
+      .select(players('player1))
+      .union(players.select(players('player2)))
+      .collect()
+      .run()
   }
 
 }
