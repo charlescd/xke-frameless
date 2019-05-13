f1fc3d3d8be6ad6186a8f031801852039c794ee8
Charles Dufour <cdufour@xebia.fr>
2019-05-13 07:16:25 +0200

    exercice2

--- src/main/scala/fr/xebia/xke/frameless/Exercice2.scala
+++ src/main/scala/fr/xebia/xke/frameless/Exercice2.scala
@@ -21,8 +21,8 @@ object Exercice2 {
         )
 
     playersDataset
-      .groupBy("player")
-      .agg((max(col("duration")) / 1000L).cast(DataTypes.StringType).as("max"))
+      .groupBy("player1")
+      .agg((max(col("duration")) / 1000L).cast(DataTypes.LongType).as("max"))
       .orderBy(col("max").desc)
       .collect()
       .map(row => DurationByPlayer(row.getAs[String]("player1"), row.getAs[Long]("max")))
@@ -30,7 +30,23 @@ object Exercice2 {
   }
 
   def maxDurationTyped(matchs: Dataset[Match]): DurationByPlayer = {
-    ???
+    val typedMatchs = TypedDataset.create(matchs)
+
+    val players1 = typedMatchs.select(typedMatchs('player1), typedMatchs('duration)).as[DurationByPlayer]
+    val players2 = typedMatchs.select(typedMatchs('player2), typedMatchs('duration)).as[DurationByPlayer]
+
+    val players = players1.union(players2)
+
+    val durations = players
+      .groupBy(players('player))
+      .agg((max(players('duration)) / 1000L).cast[Long])
+      .as[DurationByPlayer]
+
+    durations
+      .orderBy(durations('duration).desc)
+      .collect()
+      .run()
+      .head
   }
 
 }
